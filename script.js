0fc8266888d4ab09acb220f25cda3bff0822a3d6
Diazzstr <MMZDiazzstr@users.noreply.github.com>
2019-05-27 17:21:57 +0300

    3-rd commit

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-let timeend;
-timeend = new Date (2019, 05, 26, 24, 00);
+
+let timeend = new Date (2022, 04, 28, 12, 00);
 function time() {
-    today = new Date();
-    today = Math.floor((timeend-today)/1000);
-    tmin = today%60;
+    let today = new Date();
+    today = Math.floor((timeend - today)/1000);
+    today = Math.floor(today/60);
+    let tmin = Math.floor(today%60);
     today = Math.floor(today/60);
     if (tmin < 10) tmin = '0' + tmin;
-    thour = today%24;
+    let thour = Math.floor(today%24);
     today = Math.floor(today/24);
+    
     timestr1 = today;
     timestr2 = thour;
     timestr3 = tmin;
